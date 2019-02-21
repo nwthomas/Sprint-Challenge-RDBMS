1d6f92f4c0a8054dc9e4ec0a3cd92b76a6f7e233
Nathan Thomas <nwthomas@me.com>
2019-02-20 22:02:30 -0800

    Add .post() request for submitting projects

--- server.js
+++ server.js
@@ -15,4 +15,60 @@ server.get("/", (req, res) => {
   res.send("Dude!");
 });
 
+server.get("/api/projects", async (req, res) => {
+  try {
+    const projects = await db("projects");
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error retrieving the projects from the database.",
+      error
+    });
+  }
+});
+
+server.get("/api/projects/:id", async (req, res) => {
+  try {
+    const project = await db("projects").where({ id: req.params.id });
+    if (project.length) {
+      res.status(200).json(project);
+    } else {
+      res
+        .status(404)
+        .json({ message: "That project could not be found in the database." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error retrieving the project from the database.",
+      error
+    });
+  }
+});
+
+server.post("/api/projects", async (req, res) => {
+  if (!req.body.name || !req.body.description) {
+    return res.status(400).json({
+      message:
+        "Please include a name, completion status, and description, and try again."
+    });
+  }
+  try {
+    const project = await db("projects").insert(req.body);
+    if (project) {
+      res
+        .status(200)
+        .json({ message: "Project created successfully.", project });
+    } else {
+      res
+        .status(404)
+        .json({ message: "The project could not be added to the database." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error adding the project to the database.",
+      error
+    });
+  }
+});
+
 module.exports = server;
